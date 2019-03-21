58c5278e9b17f3a3a336487f313a3bf67e1fd209
Eva <evangeline013@gmail.com>
2019-03-20 20:58:36 -0400

    install dependencies,update postgres username and pwd, and change header style

--- config/dev.exs
+++ config/dev.exs
@@ -36,8 +36,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :forum, Forum.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "Evangeline",
+  password: "",
   database: "forum_dev",
   hostname: "localhost",
   pool_size: 10
